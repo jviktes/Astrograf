b09501e8e4b06a1dff00cd5536aed35d8dddd76f
Pavel Vik <vik@noabe.com>
2021-03-20 20:59:34 +0100

    pridani lcd

--- DisplayAzimut/DisplayAzimut.ino
+++ DisplayAzimut/DisplayAzimut.ino
@@ -1,13 +1,19 @@
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_LSM303_U.h>
 #include <Adafruit_9DOF.h>
 #include <Adafruit_L3GD20_U.h>
 
+#include <LiquidCrystal_I2C.h>
+
 /* Assign a unique ID to the sensors */
 Adafruit_9DOF                 dof   = Adafruit_9DOF();
 Adafruit_LSM303_Accel_Unified accel = Adafruit_LSM303_Accel_Unified(30301);
 Adafruit_LSM303_Mag_Unified   mag   = Adafruit_LSM303_Mag_Unified(30302);
+
+LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x3F for a 16 chars and 2 line display
+
 
 /**************************************************************************/
 /*!
@@ -42,6 +48,18 @@ void setup(void)
   
   /* Initialise the sensors */
   initSensors();
+
+  lcd.init();
+  lcd.clear();         
+  lcd.backlight();      // Make sure backlight is on
+  
+  // Print a message on both lines of the LCD.
+  lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
+  lcd.print("Hello world!");
+  
+  lcd.setCursor(2,1);   //Move cursor to character 2 on line 1
+  lcd.print("LCD Tutorial");
+  
 }
 
 /**************************************************************************/
@@ -51,6 +69,7 @@ void setup(void)
 /**************************************************************************/
 void loop(void)
 {
+        
   sensors_event_t accel_event;
   sensors_event_t mag_event;
   sensors_vec_t   orientation;
@@ -71,6 +90,21 @@ void loop(void)
     Serial.print(orientation.heading);
     Serial.println(F(""));
   }
+
+  lcd.setCursor(2,0);   //Set cursor to character 2 on line 0
+  lcd.print("V:"+String(orientation.pitch));
   
-  delay(100);
+  lcd.setCursor(2,1);   //Move cursor to character 2 on line 1
+  String headingCorrectAngle = "";
+  if (orientation.heading<0) {
+    headingCorrectAngle = String(orientation.heading*-1);
+  }
+  else {
+    headingCorrectAngle = String(360-orientation.heading);
+  }
+  
+  lcd.print("Az:"+String(headingCorrectAngle)); //ukazuje uhel v minus a obracene proti smeru S-->V atd.
+  
+  delay(1000);
+  lcd.clear();
 }
