24445dcceb44e078c081f77fce5a116198ce74f1
Pavel Vik <vik@noabe.com>
2021-06-16 21:15:37 +0200

    prejmenovani promenybch atd

--- Stelarium/stelarium/stelarium.ino
+++ Stelarium/stelarium/stelarium.ino
@@ -6,46 +6,47 @@
 
 // Define the number of pulses that your encoder (1 and 2) gives by turn, and multiply by 4
 // In my case: 600 x 15 x 4  (600 pulses by turn; 15 is the gear ratio), so:
-long pulses_enc1 = 36000;              
-long pulses_enc2 = 36000;             
+long PULSES_ENC_1 = 36000;              
+long PULSES_ENC_2 = 36000;             
 
 // Define DUE's pins
-#define enc_1A 2   // define DUE pin to encoder 1-channel A                     
-#define enc_1B 3   // define DUE pin to encoder 1-channel B  
-#define enc_2A 4   // define DUE pin to encoder 2-channel A  
-#define enc_2B 5   // define DUE pin to encoder 2-channel B  
+#define ENC_1A 2   // define DUE pin to encoder 1-channel A                     
+#define ENC_1B 3   // define DUE pin to encoder 1-channel B  
+#define ENC_2A 4   // define DUE pin to encoder 2-channel A  
+#define ENC_2B 5   // define DUE pin to encoder 2-channel B  
 
 // enter your latitude (example: North 40Âº33'20'')
-int latHH = 50;    // this means 40Âº North
-int latMM = 45;
-int latSS = 45;
+int POSITION_LATITUDE_HH = 50;    // this means 40Âº North
+int POSITION_LATITUDE_MM = 45;
+int lPOSITION_LATITUDE_SS = 45;
 
 // enter Pole Star right ascention (AR: HH:MM:SS)
-int poleAR_HH = 2;    // this means 2 hours, 52 minutes and 16 seconds
-int poleAR_MM = 58;
-int poleAR_SS = 30;
+int POLARKA_AR_HH = 2;    // this means 2 hours, 52 minutes and 16 seconds
+int POLARKA_AR_MM = 58;
+int POLARKA_AR_SS = 30;
 
 // enter Pole Star hour angle (H: HH:MM:SS)
-int poleH_HH = 15;
-int poleH_MM = 33;
-int poleH_SS = 24;
+int POLARKA_HOUR_CLOCK_HH = 15;
+int POLARKA_HOUR_CLOCK_MM = 33;
+int POLARKA_HOUR_CLOCK_SS = 24;
 
 SoftwareSerial mySerial(7, 8); // RX, TX - toto slouzi je pro vypis, co posilam na stelarium
 
-unsigned long seg_sideral = 1003;
-const double      pi = 3.14159265358979324;
+unsigned long SEG_SIDERAL = 1003;
+const double      PI = 3.14159265358979324;
 volatile int lastEncoded1 = 0;
 volatile long encoderValue1 = 0;
+
 volatile int lastEncoded2 = 0;
 volatile long encoderValue2 = 0;
 
-char input[20];
-//vystupem jsou txAr a txDEC = naformatovane hodnoty souradnic v EQ
-char txAR[10];
-char txDEC[11];
+char _input_from_Stelarium[20];
+//vystupem jsou _AR_Formated_Stelarium a _DEC_Formated_Stelarium = naformatovane hodnoty souradnic v EQ
+char _AR_Formated_Stelarium[10];
+char _DEC_Formated_Stelarium[11];
 
 long TSL;
-unsigned long t_ciclo_acumulado = 0, t_ciclo;
+unsigned long t_ciclo_acumulado = 0, time_running_arduino;
 
 long Az_tel_s, Alt_tel_s;
 
@@ -54,26 +55,25 @@ long AR_tel_s, DEC_tel_s;
 long AR_stell_s, DEC_stell_s;
 
 double cos_phi, sin_phi;
-double alt, azi;
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 void setup()
 {
   Serial.begin(9600);
   
-  pinMode(enc_1A, INPUT_PULLUP);
-  pinMode(enc_1B, INPUT_PULLUP);
-  pinMode(enc_2A, INPUT_PULLUP);
-  pinMode(enc_2B, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(enc_1A), Encoder1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(enc_1B), Encoder1, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(enc_2A), Encoder2, CHANGE);
-  attachInterrupt(digitalPinToInterrupt(enc_2B), Encoder2, CHANGE);
+  pinMode(ENC_1A, INPUT_PULLUP);
+  pinMode(ENC_1B, INPUT_PULLUP);
+  pinMode(ENC_2A, INPUT_PULLUP);
+  pinMode(ENC_2B, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(ENC_1A), Encoder_Interrupt_1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC_1B), Encoder_Interrupt_1, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC_2A), Encoder_Interrupt_2, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(ENC_2B), Encoder_Interrupt_2, CHANGE);
 
-  cos_phi = cos((((latHH * 3600) + (latMM * 60) + latSS) / 3600.0) * pi / 180.0);
-  sin_phi = sin((((latHH * 3600) + (latMM * 60) + latSS) / 3600.0) * pi / 180.0);
+  cos_phi = cos((((POSITION_LATITUDE_HH * 3600) + (POSITION_LATITUDE_MM * 60) + lPOSITION_LATITUDE_SS) / 3600.0) * PI / 180.0);
+  sin_phi = sin((((POSITION_LATITUDE_HH * 3600) + (POSITION_LATITUDE_MM * 60) + lPOSITION_LATITUDE_SS) / 3600.0) * PI / 180.0);
 
-  TSL = poleAR_HH * 3600 + poleAR_MM * 60 + poleAR_SS + poleH_HH * 3600 + poleH_MM * 60 + poleH_SS;
+  TSL = POLARKA_AR_HH * 3600 + POLARKA_AR_MM * 60 + POLARKA_AR_SS + POLARKA_HOUR_CLOCK_HH * 3600 + POLARKA_HOUR_CLOCK_MM * 60 + POLARKA_HOUR_CLOCK_SS;
   while (TSL >= 86400) TSL = TSL - 86400;
 
 
@@ -88,10 +88,10 @@ void setup()
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 void loop()
 {
-  t_ciclo = millis(); //cas od spusteni arduina
-  if (t_ciclo_acumulado >= seg_sideral) {
+  time_running_arduino = millis(); //cas od spusteni arduina
+  if (t_ciclo_acumulado >= SEG_SIDERAL) {
     TSL++;
-    t_ciclo_acumulado = t_ciclo_acumulado - seg_sideral;
+    t_ciclo_acumulado = t_ciclo_acumulado - SEG_SIDERAL;
     if (TSL >= 86400) {
       TSL = TSL - 86400;
     }
@@ -103,8 +103,8 @@ void loop()
   if (Serial.available() > 0) 
   {communication();}
 
-  t_ciclo = millis() - t_ciclo;
-  t_ciclo_acumulado = t_ciclo_acumulado + t_ciclo;
+  time_running_arduino = millis() - time_running_arduino;
+  t_ciclo_acumulado = t_ciclo_acumulado + time_running_arduino;
   
 }
 
@@ -113,65 +113,68 @@ void communication()
 {
   mySerial.write("communication");
   int i = 0;
-  input[i++] = Serial.read();
+  _input_from_Stelarium[i++] = Serial.read();
   delay(5);
-  while ((input[i++] = Serial.read()) != '#') {
+  while ((_input_from_Stelarium[i++] = Serial.read()) != '#') {
     delay(5);
   }
-  input[i] = '\0';
+  _input_from_Stelarium[i] = '\0';
 
-    mySerial.write("txAR");
-    mySerial.write(txAR);
-    mySerial.write("txDEC");
-    mySerial.write(txDEC);
+    mySerial.write("_AR_Formated_Stelarium");
+    mySerial.write(_AR_Formated_Stelarium);
+    mySerial.write("_DEC_Formated_Stelarium");
+    mySerial.write(_DEC_Formated_Stelarium);
     
-  if (input[1] == ':' && input[2] == 'G' && input[3] == 'R' && input[4] == '#') {
-    Serial.print(txAR);
+  if (_input_from_Stelarium[1] == ':' && _input_from_Stelarium[2] == 'G' && _input_from_Stelarium[3] == 'R' && _input_from_Stelarium[4] == '#') {
+    Serial.print(_AR_Formated_Stelarium);
 
   }
 
-  if (input[1] == ':' && input[2] == 'G' && input[3] == 'D' && input[4] == '#') {
-    Serial.print(txDEC);
+  if (_input_from_Stelarium[1] == ':' && _input_from_Stelarium[2] == 'G' && _input_from_Stelarium[3] == 'D' && _input_from_Stelarium[4] == '#') {
+    Serial.print(_DEC_Formated_Stelarium);
 
   }
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 void read_sensors() {
-  long h_deg, h_min, h_seg, A_deg, A_min, A_seg;
 
-  if (encoderValue2 >= pulses_enc2 || encoderValue2 <= -pulses_enc2) {
+    //fake:
+    encoderValue2= encoderValue2+200;
+
+  if (encoderValue2 >= PULSES_ENC_2 || encoderValue2 <= -PULSES_ENC_2) {
     encoderValue2 = 0;
   }
   
-  int enc1 = encoderValue1 / 1500;
-  long encoder1_temp = encoderValue1 - (enc1 * 1500);
-  long map1 = enc1 * map(1500, 0, pulses_enc1, 0, 324000); //map(value, fromLow, fromHigh, toLow, toHigh)
-  
-  int enc2 = encoderValue2 / 1500;
-  long encoder2_temp = encoderValue2 - (enc2 * 1500);
-  long map2 = enc2 * map(1500, 0, pulses_enc2, 0, 1296000);
+  int _enc_1 = encoderValue1 / 1500;
+  long encoder1_temp = encoderValue1 - (_enc_1 * 1500);
+  long map1 = _enc_1 * map(1500, 0, PULSES_ENC_1, 0, 324000); //map(value, fromLow, fromHigh, toLow, toHigh)
+  Alt_tel_s = map1 + map(encoder1_temp, 0, PULSES_ENC_1, 0, 324000);
 
-  Alt_tel_s = map1 + map (encoder1_temp, 0, pulses_enc1, 0, 324000);
-  Az_tel_s  = map2 + map (encoder2_temp, 0, pulses_enc2, 0, 1296000);
+
+  int _enc_2 = encoderValue2 / 1500;
+  long encoder2_temp = encoderValue2 - (_enc_2 * 1500);
+
+  long map2 = _enc_2 * map(1500, 0, PULSES_ENC_2, 0, 1296000);
+  Az_tel_s  = map2 + map (encoder2_temp, 0, PULSES_ENC_2, 0, 1296000);
 
   if (Az_tel_s < 0) Az_tel_s = 1296000 + Az_tel_s;
   if (Az_tel_s >= 1296000) Az_tel_s = Az_tel_s - 1296000 ;
 
 }
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------
-void Encoder1() {
-  int encoded1 = (digitalRead(enc_1A) << 1) | digitalRead(enc_1B);
+//Metoda volaná pri zmene v pulzu, preruseni:
+void Encoder_Interrupt_1() {
+  int encoded1 = (digitalRead(ENC_1A) << 1) | digitalRead(ENC_1B);
   int sum  = (lastEncoded1 << 2) | encoded1;
   if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue1 ++;
   if (sum == 0b1110 || sum == 0b0111 || sum == 0b0001 || sum == 0b1000) encoderValue1 --;
   lastEncoded1 = encoded1;
 }
 
-//--------------------------------------------------------------------------------------------------------------------------------------------------------
-void Encoder2() {
-  int encoded2 = (digitalRead(enc_2A) << 1) | digitalRead(enc_2B);
+//Metoda volaná pri zmene v pulzu, preruseni:
+void Encoder_Interrupt_2() {
+  int encoded2 = (digitalRead(ENC_2A) << 1) | digitalRead(ENC_2B);
   int sum  = (lastEncoded2 << 2) | encoded2;
 
   if (sum == 0b1101 || sum == 0b0100 || sum == 0b0010 || sum == 0b1011) encoderValue2 ++;
@@ -180,7 +183,7 @@ void Encoder2() {
 }
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
-//vystupem jsou txAr a txDEC = naformatovane hodnoty souradnic v EQ
+//vystupem jsou _AR_Formated_Stelarium a _DEC_Formated_Stelarium = naformatovane hodnoty souradnic v EQ
 void AZ_to_EQ()
 {
   double delta_tel, sin_h, cos_h, sin_A, cos_A, sin_DEC, cos_DEC;
@@ -190,8 +193,8 @@ void AZ_to_EQ()
   long decDEG, decMM, decSS;
   char sDEC_tel;
 
-  A_telRAD = (Az_tel_s / 3600.0) * pi / 180.0;
-  h_telRAD = (Alt_tel_s / 3600.0) * pi / 180.0;
+  A_telRAD = (Az_tel_s / 3600.0) * PI / 180.0;
+  h_telRAD = (Alt_tel_s / 3600.0) * PI / 180.0;
   sin_h = sin(h_telRAD);
   cos_h = cos(h_telRAD);
   sin_A = sin(A_telRAD);
@@ -199,7 +202,7 @@ void AZ_to_EQ()
   delta_tel = asin((sin_phi * sin_h) + (cos_phi * cos_h * cos_A));
   sin_DEC = sin(delta_tel);
   cos_DEC = cos(delta_tel);
-  DEC_tel_s = long((delta_tel * 180.0 / pi) * 3600.0);
+  DEC_tel_s = long((delta_tel * 180.0 / PI) * 3600.0);
 
   while (DEC_tel_s >= 324000) {
     DEC_tel_s = DEC_tel_s - 324000;
@@ -209,7 +212,7 @@ void AZ_to_EQ()
   }
 
   H_telRAD = acos((sin_h - (sin_phi * sin_DEC)) / (cos_phi * cos_DEC));
-  H_tel = long((H_telRAD * 180.0 / pi) * 240.0);
+  H_tel = long((H_telRAD * 180.0 / PI) * 240.0);
 
   if (sin_A >= 0) {
     H_tel = 86400 - H_tel;
@@ -231,8 +234,8 @@ void AZ_to_EQ()
   decSS = (abs(DEC_tel_s) - decDEG * 3600) - decMM * 60;
   (DEC_tel_s < 0) ? sDEC_tel = 45 : sDEC_tel = 43;
 
-  //vystupem jsou txAr a txDEC = naformatovane hodnoty souradnic v EQ
-  sprintf(txAR, "%02d:%02d:%02d#", int(arHH), int(arMM), int(arSS));
-  sprintf(txDEC, "%c%02d%c%02d:%02d#", sDEC_tel, int(decDEG), 223, int(decMM), int(decSS));
+  //vystupem jsou _AR_Formated_Stelarium a _DEC_Formated_Stelarium = naformatovane hodnoty souradnic v EQ
+  sprintf(_AR_Formated_Stelarium, "%02d:%02d:%02d#", int(arHH), int(arMM), int(arSS));
+  sprintf(_DEC_Formated_Stelarium, "%c%02d%c%02d:%02d#", sDEC_tel, int(decDEG), 223, int(decMM), int(decSS));
   
 }
