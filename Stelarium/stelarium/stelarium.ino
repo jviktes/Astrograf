1dbdb9cf11cadb59e2c62f631ad2852e1506b6c6
jviktes <jviktes@gmail.com>
2021-06-17 08:11:45 +0200

    Add files via upload

--- Stelarium/stelarium/stelarium.ino
+++ Stelarium/stelarium/stelarium.ino
@@ -6,8 +6,8 @@
 
 // Define the number of pulses that your encoder (1 and 2) gives by turn, and multiply by 4
 // In my case: 600 x 15 x 4  (600 pulses by turn; 15 is the gear ratio), so:
-long PULSES_ENC_1 = 36000;              
-long PULSES_ENC_2 = 36000;             
+long TOTAL_PULSES_PER_ROUND_ENC_1 = 36000;              
+long TOTAL_PULSES_PER_ROUND_ENC_2 = 36000;             
 
 // Define DUE's pins
 #define ENC_1A 2   // define DUE pin to encoder 1-channel A                     
@@ -15,7 +15,8 @@ long PULSES_ENC_2 = 36000;
 #define ENC_2A 4   // define DUE pin to encoder 2-channel A  
 #define ENC_2B 5   // define DUE pin to encoder 2-channel B  
 
-// enter your latitude (example: North 40Âº33'20'')
+// enter your latitude (example: North 40Âº33'20'') 
+//Longitude will be calculated through AR and H.
 int POSITION_LATITUDE_HH = 50;    // this means 40Âº North
 int POSITION_LATITUDE_MM = 45;
 int lPOSITION_LATITUDE_SS = 45;
@@ -33,7 +34,8 @@ int POLARKA_HOUR_CLOCK_SS = 24;
 SoftwareSerial mySerial(7, 8); // RX, TX - toto slouzi je pro vypis, co posilam na stelarium
 
 unsigned long SEG_SIDERAL = 1003;
-const double      PI = 3.14159265358979324;
+const double      CONST_PI = 3.14159265358979324;
+
 volatile int lastEncoded1 = 0;
 volatile long encoderValue1 = 0;
 
@@ -52,8 +54,6 @@ long Az_tel_s, Alt_tel_s;
 
 long AR_tel_s, DEC_tel_s;
 
-long AR_stell_s, DEC_stell_s;
-
 double cos_phi, sin_phi;
 
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -70,8 +70,8 @@ void setup()
   attachInterrupt(digitalPinToInterrupt(ENC_2A), Encoder_Interrupt_2, CHANGE);
   attachInterrupt(digitalPinToInterrupt(ENC_2B), Encoder_Interrupt_2, CHANGE);
 
-  cos_phi = cos((((POSITION_LATITUDE_HH * 3600) + (POSITION_LATITUDE_MM * 60) + lPOSITION_LATITUDE_SS) / 3600.0) * PI / 180.0);
-  sin_phi = sin((((POSITION_LATITUDE_HH * 3600) + (POSITION_LATITUDE_MM * 60) + lPOSITION_LATITUDE_SS) / 3600.0) * PI / 180.0);
+  cos_phi = cos((((POSITION_LATITUDE_HH * 3600) + (POSITION_LATITUDE_MM * 60) + lPOSITION_LATITUDE_SS) / 3600.0) * CONST_PI / 180.0);
+  sin_phi = sin((((POSITION_LATITUDE_HH * 3600) + (POSITION_LATITUDE_MM * 60) + lPOSITION_LATITUDE_SS) / 3600.0) * CONST_PI / 180.0);
 
   TSL = POLARKA_AR_HH * 3600 + POLARKA_AR_MM * 60 + POLARKA_AR_SS + POLARKA_HOUR_CLOCK_HH * 3600 + POLARKA_HOUR_CLOCK_MM * 60 + POLARKA_HOUR_CLOCK_SS;
   while (TSL >= 86400) TSL = TSL - 86400;
@@ -111,10 +111,16 @@ void loop()
 //--------------------------------------------------------------------------------------------------------------------------------------------------------
 void communication()
 {
-  mySerial.write("communication");
+  mySerial.write("communication:");
+
   int i = 0;
+
   _input_from_Stelarium[i++] = Serial.read();
   delay(5);
+
+
+  //While connected to Arduino, periodically Stellarium sends 2 strings: ":GR#" - ready to receive RA and ":GD#" - to receive DEC.
+
   while ((_input_from_Stelarium[i++] = Serial.read()) != '#') {
     delay(5);
   }
@@ -132,7 +138,6 @@ void communication()
 
   if (_input_from_Stelarium[1] == ':' && _input_from_Stelarium[2] == 'G' && _input_from_Stelarium[3] == 'D' && _input_from_Stelarium[4] == '#') {
     Serial.print(_DEC_Formated_Stelarium);
-
   }
 }
 
@@ -141,22 +146,24 @@ void read_sensors() {
 
     //fake:
     encoderValue2= encoderValue2+200;
+    delay(1000);
 
-  if (encoderValue2 >= PULSES_ENC_2 || encoderValue2 <= -PULSES_ENC_2) {
+  if (encoderValue2 >= TOTAL_PULSES_PER_ROUND_ENC_2 || encoderValue2 <= -TOTAL_PULSES_PER_ROUND_ENC_2) {
     encoderValue2 = 0;
   }
   
+  //Alta teleskop:
   int _enc_1 = encoderValue1 / 1500;
   long encoder1_temp = encoderValue1 - (_enc_1 * 1500);
-  long map1 = _enc_1 * map(1500, 0, PULSES_ENC_1, 0, 324000); //map(value, fromLow, fromHigh, toLow, toHigh)
-  Alt_tel_s = map1 + map(encoder1_temp, 0, PULSES_ENC_1, 0, 324000);
+  long map1 = _enc_1 * map(1500, 0, TOTAL_PULSES_PER_ROUND_ENC_1, 0, 324000); //map(value, fromLow, fromHigh, toLow, toHigh)
+  Alt_tel_s = map1 + map(encoder1_temp, 0, TOTAL_PULSES_PER_ROUND_ENC_1, 0, 324000);
 
-
+  //Azimut teleskop:
   int _enc_2 = encoderValue2 / 1500;
   long encoder2_temp = encoderValue2 - (_enc_2 * 1500);
 
-  long map2 = _enc_2 * map(1500, 0, PULSES_ENC_2, 0, 1296000);
-  Az_tel_s  = map2 + map (encoder2_temp, 0, PULSES_ENC_2, 0, 1296000);
+  long map2 = _enc_2 * map(1500, 0, TOTAL_PULSES_PER_ROUND_ENC_2, 0, 1296000);
+  Az_tel_s  = map2 + map (encoder2_temp, 0, TOTAL_PULSES_PER_ROUND_ENC_2, 0, 1296000);
 
   if (Az_tel_s < 0) Az_tel_s = 1296000 + Az_tel_s;
   if (Az_tel_s >= 1296000) Az_tel_s = Az_tel_s - 1296000 ;
@@ -193,8 +200,8 @@ void AZ_to_EQ()
   long decDEG, decMM, decSS;
   char sDEC_tel;
 
-  A_telRAD = (Az_tel_s / 3600.0) * PI / 180.0;
-  h_telRAD = (Alt_tel_s / 3600.0) * PI / 180.0;
+  A_telRAD = (Az_tel_s / 3600.0) * CONST_PI / 180.0;
+  h_telRAD = (Alt_tel_s / 3600.0) * CONST_PI / 180.0;
   sin_h = sin(h_telRAD);
   cos_h = cos(h_telRAD);
   sin_A = sin(A_telRAD);
@@ -202,7 +209,7 @@ void AZ_to_EQ()
   delta_tel = asin((sin_phi * sin_h) + (cos_phi * cos_h * cos_A));
   sin_DEC = sin(delta_tel);
   cos_DEC = cos(delta_tel);
-  DEC_tel_s = long((delta_tel * 180.0 / PI) * 3600.0);
+  DEC_tel_s = long((delta_tel * 180.0 / CONST_PI) * 3600.0);
 
   while (DEC_tel_s >= 324000) {
     DEC_tel_s = DEC_tel_s - 324000;
@@ -212,7 +219,7 @@ void AZ_to_EQ()
   }
 
   H_telRAD = acos((sin_h - (sin_phi * sin_DEC)) / (cos_phi * cos_DEC));
-  H_tel = long((H_telRAD * 180.0 / PI) * 240.0);
+  H_tel = long((H_telRAD * 180.0 / CONST_PI) * 240.0);
 
   if (sin_A >= 0) {
     H_tel = 86400 - H_tel;
