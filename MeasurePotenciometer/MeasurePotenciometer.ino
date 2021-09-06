7bde79e8cd4a5252d0b56460259dee4d43b41105
Pavel Vik <vik@noabe.com>
2021-09-06 19:47:18 +0200

    definice vytupniho formatu z arduina

--- MeasurePotenciometer/MeasurePotenciometer.ino
+++ MeasurePotenciometer/MeasurePotenciometer.ino
@@ -9,14 +9,21 @@ void loop() {
   val_azimut = analogRead(A0);    // read the value from the sensor
   double uhel_azimut = (450*val_azimut)/1024;
   //Serial.println("Azimut:");
-  Serial.println(analogRead(A0));//TODO - toto surove funguje, stelarium neumi desetiine carky atd.
-  //Serial.println(uhel_azimut);
-//
-//  val_alt = analogRead(A1)-590;    // read the value from the sensor
-//  double uhel_alt = (450*val_alt)/1024;
+  //Toto funguje pro cela cisla:
+  //Serial.println(analogRead(A0));//TODO - toto surove funguje, stelarium neumi desetiine carky atd.
+
+  val_alt = analogRead(A1)-590;    // read the value from the sensor
+  double uhel_alt = (450*val_alt)/1024;
+  uhel_alt = 42.52;
 //  Serial.println("Alt:");
 //  Serial.println(analogRead(A1));
 //  Serial.println(uhel_alt);
+
+  Serial.println("az:"+String(uhel_azimut)+"|al:"+String(uhel_alt));
+  
+  //Serial.println(uhel_azimut);
+//
+
 //  
   delay(100);
 }
