466ea4dbc911d88505f323c8585229cc416a5dcd
Pavel Vik <vik@noabe.com>
2021-09-11 13:26:34 +0200

    upravy pro realne pouziti

--- StepMotors/stepMotor_A4988_astrograf/stepMotor_A4988_astrograf.ino
+++ StepMotors/stepMotor_A4988_astrograf/stepMotor_A4988_astrograf.ino
@@ -1,40 +1,83 @@
 //test prepisu
+//https://lastminuteengineers.com/a4988-stepper-motor-driver-arduino-tutorial/
 // Define pin connections & motor's steps per revolution
 const int dirPin = 2;
 const int stepPin = 3;
-const int stepsPerRevolution = 3200;
-const int transmittionRate = 5;
+ int stepsPerRevolution = 1600; //200 = pro nastaveni MS1=0, MS2=0, MS3=0
+const int transmittionRate = 1; //toto je pro prevod na vystupu
 const int stepDelay = 5400; //5.4s = 5400ms = 5400 000 milisec
 const int delayPause = 2700000;
 
 void setup()
 {
+  Serial.begin(9600);
   // Declare pins as Outputs
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
   pinMode(6, OUTPUT); //Enable
   digitalWrite(6,LOW);   
-  
-}
-void loop()
-{
-  // Set motor direction clockwise
+
+    // Set motor direction clockwise
   digitalWrite(dirPin, HIGH);
 
+  Serial.println("Start...");
+
   // Spin motor slowly
+
+  //potrebuju natočit motor o několik stupnu:
+  //vstupy: aktualni uhel, rychlost, smer
+  
   for(int x = 0; x < stepsPerRevolution*transmittionRate; x++)
   {
+    Serial.println(x);
     digitalWrite(stepPin, HIGH);
     //delayMicroseconds(delayPause);
-    delay(2700);
+    delay(10);
     digitalWrite(stepPin, LOW);
     //delayMicroseconds(delayPause);delay(2700);
-    delay(2700);
+    delay(10);
   }
+  Serial.println("End...");
   delay(1000); // Wait a second
   
+}
+
+void MoveTo(int numberSteps) {
+
+  for(int x = 0; x < stepsPerRevolution*transmittionRate; x++)
+  {
+    Serial.println(x);
+    digitalWrite(stepPin, HIGH);
+    //delayMicroseconds(delayPause);
+    delay(10);
+    digitalWrite(stepPin, LOW);
+    //delayMicroseconds(delayPause);delay(2700);
+    delay(10);
+  }
+  
+}
+
+void loop()
+{
+//  // Set motor direction clockwise
+//  digitalWrite(dirPin, HIGH);
+//
+//  // Spin motor slowly
+//  stepsPerRevolution=200;
+//  for(int x = 0; x < stepsPerRevolution*transmittionRate; x++)
+//  {
+//    Serial.println("Krok c.:"+x);
+//    digitalWrite(stepPin, HIGH);
+//    //delayMicroseconds(delayPause);
+//    delay(100);
+//    digitalWrite(stepPin, LOW);
+//    //delayMicroseconds(delayPause);delay(2700);
+//    delay(100);
+//  }
+//  delay(1000); // Wait a second
+  
   // Set motor direction counterclockwise
-  digitalWrite(dirPin, LOW);
+  //digitalWrite(dirPin, LOW);
 
   // Spin motor quickly
 //  for(int x = 0; x < stepsPerRevolution; x++)
